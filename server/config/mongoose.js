facb549f5ab78bf3e71e5d87a6b2841597f5eb80
Tiffany Thompson <tdthompson899@gmail.com>
2017-04-27 13:47:27 -0500

    removed the space in mongoose.js for mongodb connection credentials; installed and saved mongodb via npm install but later removed

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 var fs = require("fs");
 
 // mongoose.connect("mongodb://localhost/mean_store");
-mongoose.connect("mongodb: //heroku_n2js56gg:ugrebafsg1tagvfrhp08jn2a1c@ds121091.mlab.com:21091/heroku_n2js56gg");
+mongoose.connect("mongodb://heroku_n2js56gg:ugrebafsg1tagvfrhp08jn2a1c@ds121091.mlab.com:21091/heroku_n2js56gg");
 var models_path = __dirname + "/../models"
 
 fs.readdirSync(models_path).forEach(function(file){
